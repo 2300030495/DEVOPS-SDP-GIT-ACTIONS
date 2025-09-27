798c6fe142a23518a7e40c9f17ced6d9c0111549
bhavyavadlani <2300032288cser@gmail.com>
2025-09-27 09:00:57 +0530

    Update AdminRepository.java

--- SDPProject/src/main/java/com/klef/fsd/repository/AdminRepository.java
+++ SDPProject/src/main/java/com/klef/fsd/repository/AdminRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import com.klef.fsd.model.Admin;
 
+// this is admin repo all admin functionality will be inbuilt here
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, String> {
 	public Admin findByUsernameAndPassword(String username, String password);
